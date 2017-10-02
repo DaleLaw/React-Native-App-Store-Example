ce82bac75b5cf1bec4cae984c5ac18f43ab6b7e5
Dale Law <developerdalelaw@gmail.com>
2017-10-03 02:31:03 +0800

    Fix Redux import style

--- App/Redux/index.js
+++ App/Redux/index.js
@@ -1,14 +1,17 @@
 import { combineReducers } from 'redux'
 import configureStore from './CreateStore'
 import rootSaga from '../Sagas/'
+import FreeAppsReducer from './reducers/FreeAppsReducer'
+import RecommendationsReducer from './reducers/RecommendationsReducer'
+import SearchReducer from './reducers/SearchReducer'
 
 export default () => {
   /* ------------- Assemble The Reducers ------------- */
   const rootReducer = combineReducers({
     nav: require('./NavigationRedux').reducer, // eslint-disable-line global-require
-    freeApps: require('./reducers/FreeAppsReducer'), // eslint-disable-line global-require
-    recommendations: require('./reducers/RecommendationsReducer'), // eslint-disable-line global-require
-    search: require('./reducers/SearchReducer'), // eslint-disable-line global-require
+    freeApps: FreeAppsReducer, // eslint-disable-line global-require
+    recommendations: RecommendationsReducer, // eslint-disable-line global-require
+    search: SearchReducer, // eslint-disable-line global-require
   })
 
   return configureStore(rootReducer, rootSaga)
