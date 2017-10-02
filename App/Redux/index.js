ee77859939a760e8048422d04bf8a19e3d1f2c49
Dale Law <developerdalelaw@gmail.com>
2017-10-03 00:26:47 +0800

    Add  reducers to Redux

--- App/Redux/index.js
+++ App/Redux/index.js
@@ -6,6 +6,9 @@ export default () => {
   /* ------------- Assemble The Reducers ------------- */
   const rootReducer = combineReducers({
     nav: require('./NavigationRedux').reducer, // eslint-disable-line global-require
+    freeApps: require('./reducers/FreeAppsReducer'), // eslint-disable-line global-require
+    recommendations: require('./reducers/RecommendationsReducer'), // eslint-disable-line global-require
+    search: require('./reducers/SearchReducer'), // eslint-disable-line global-require
   })
 
   return configureStore(rootReducer, rootSaga)
